95c6bd0b22f49517d0c20a00788b7888cac2bf15
jcsodelta@gmail.com <jcsodelta@gmail.com@d4f37bba-5cff-9f0e-6b87-5b466f62e10a>
2011-11-24 13:23:28 +0000

    Estrutura basica do programa com arvore (agora sim)

---
+++ trunk/arvore.cpp
@@ -0,0 +1,29 @@
+#include "arvore.h"
+
+Arvore::Arvore(){
+	
+}
+
+void Arvore::inserir(){
+	cout << "inserido com sucesso" << endl;
+}
+
+void Arvore::buscar(){
+	cout << "elemento encontrado!" << endl;
+}
+
+void Arvore::buscarIntervalo(){
+	cout << "intervalo encontrado" << endl;
+}
+
+void Arvore::esvaziar(){
+	cout << "arvore vazia" << endl;
+}
+
+void Arvore::imprimir(){
+	cout << endl << "\t\t" << "A R V O R E" << endl;
+}
+
+Arvore::~Arvore(){
+	
+}
